a59b4eb69cd7334a73da9e087a09709be12aad6d
490488546@qq.comm <490488546@qq.com>
2018-07-20 23:16:30 +0800

    update config.php

--- project/config/config.php
+++ project/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'my.blog.com';
+$config['base_url'] = $_SERVER['HTTP_HOST'];
 
 /*
 |--------------------------------------------------------------------------
